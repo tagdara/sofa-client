77c2a560aa4ed119dde426658d80a816e8944cae
tagdara <github@dayton.tech>
2021-11-13 21:49:50 -0800

    monitor button fix

--- src/devices/Computer/MonitorButtonStack.js
+++ src/devices/Computer/MonitorButtonStack.js
@@ -51,10 +51,6 @@ const MonitorButtonStack = props => {
     const classes = useStyles();
     const outlet = useDeviceStateStore( state => state.deviceStates[props.outletEndpointId])
     const monitor = useDeviceStateStore( state => state.deviceStates[props.endpointId])
-    const monitorDefault = monitor && monitor.Input.mode.value === props.defaultInput
-    const monitorOff = monitor.Input.mode.value === "Input.I8"
-    const outletOn = outlet && outlet.PowerController.powerState.value === "ON"
-    const outletStandby = outlet && outlet.hasOwnProperty('Energy Level') && outlet['Energy Level'].mode.value === "Standby"
 
     useEffect(() => {
         register([props.endpointId, props.outletEndpointId], 'MonitorButton-'+props.endpointId)
@@ -66,6 +62,12 @@ const MonitorButtonStack = props => {
 
 
     if (!outlet || !monitor) {  return null }
+
+    const monitorDefault = monitor && monitor.Input.mode.value === props.defaultInput
+    const monitorOff = monitor.Input.mode.value === "Input.I8"
+    const outletOn = outlet && outlet.PowerController.powerState.value === "ON"
+    const outletStandby = outlet && outlet.hasOwnProperty('Energy Level') && outlet['Energy Level'].mode.value === "Standby"
+
 
     function toggleInput(devicename) {
         if (monitorDefault) {

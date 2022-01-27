0e8d77a4484633ffed68c930e8990c190aaebddf
tagdara <github@dayton.tech>
2022-01-27 15:27:28 -0800

    change to protect ui for live view

--- src/devices/Camera/UnifiProtectButton.js
+++ src/devices/Camera/UnifiProtectButton.js
@@ -10,7 +10,8 @@ const UnifiProtectButton = props => {
     const os = useOs();
 
     function openProtect() {
-        var newurl="https://unifi.ui.com/dashboard"
+        //var newurl="https://unifi.ui.com/dashboard"
+        var newurl = "https://unifi.ui.com/device/057da3b8-6e25-480c-aa75-b605aadc8d92:1432712978/protect/liveview/5ef7ab88015aca03e700042d"
         if (os === "ios") {
             newurl = "unifi-protect://placeholder"
         }

f456050b1b2ba285a94572e6661c85b219511892
tagdara <github@dayton.tech>
2022-03-19 15:05:38 -0700

    fix to computer on list horizontal align

--- src/devices/Computer/ComputerOnList.js
+++ src/devices/Computer/ComputerOnList.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { sortByName, endpointIdByFriendlyName, endpointIdsByDisplayCategory } from 'store/deviceHelpers';
 import ComputerBadge from 'devices/Computer/ComputerBadge';
-
+import { Group } from '@mantine/core'
 
 const ComputerOnList = props => {
     const computers = sortByName(endpointIdsByDisplayCategory('COMPUTER'))
     const outletMap = {"pc1": "PC1 outlet", "pc2": "PC2 outlet", "pc3" :"PC3 outlet", "pc4": "PC4 outlet"}
 
     return (
-        <>
+        <Group spacing="xs">
             { computers.map(endpointId => 
                 <ComputerBadge key={endpointId} endpointId={ endpointId } outlet={endpointIdByFriendlyName(outletMap[endpointId])} />
             )}
-        </>
+        </Group>
     );
 }
 

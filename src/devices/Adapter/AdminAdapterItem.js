436ab9e40dad7812f326dfaf5c62d11256f55ec7
tagdara <github@dayton.tech>
2022-05-08 21:06:22 +0000

    adapter restart feature

---
+++ src/devices/Adapter/AdminAdapterItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ActionIcon } from '@mantine/core'
+import { Layers, RotateCcw } from 'react-feather'
+import { SplitButtonGroup, SplitButton } from 'components/SplitButton'
+
+export default function AdapterItem(props) { 
+
+    return (
+        <SplitButtonGroup onClick={ () => props.restartAdapter(props.adapter.name)}>
+            <SplitButton >
+                <ActionIcon>
+                    <Layers size={20} />
+                </ActionIcon>               
+            </SplitButton>
+            <SplitButton  label={ props.adapter.name } />
+            <SplitButton>
+                <ActionIcon>
+                    <RotateCcw size={20} />
+                </ActionIcon>
+            </SplitButton>
+        </SplitButtonGroup>
+    );
+}
+
--- src/layout/pages/AdapterLayout.js
+++ src/layout/pages/AdapterLayout.js
@@ -1,18 +1,34 @@
-import React from "react";
-
-import { sortByName, endpointIdsByDisplayCategory } from 'store/deviceHelpers';
-import AdapterItem from 'devices/Adapter/AdapterItem';
+import React, {useEffect, useState} from "react";
+import AdapterItem from 'devices/Adapter/AdminAdapterItem';
 import PageFrame from 'layout/PageFrame'
+import { tokenFetch } from 'store/tokenFetch'
 
 export default function AdapterLayout(props) {
 
-    const adapters = endpointIdsByDisplayCategory('ADAPTER')
-    const sortedAdapters = sortByName(adapters)
+    const [ adapters, setAdapters] = useState({})
+
+    useEffect(() => {
+        tokenFetch('/activations')
+            .then(result => setAdapters(result))
+
+    // eslint-disable-next-line 
+    }, [])    
+
+    const restartAdapter = adapterName => {
+        return tokenFetch('/activations/restart/'+adapterName)
+    }
+
+    if (!adapters || !adapters.hasOwnProperty('activated')) { return null }
+
+    console.log('adapters', adapters)
 
     return (
         <PageFrame title={"Adapters"}>
-            { sortedAdapters.map( adapter => 
-                <AdapterItem key={adapter} endpointId={adapter} />
+            { adapters.activated.map( adapter => 
+                <AdapterItem adapter={adapter} restartAdapter={restartAdapter} />
+            )}
+            { adapters.pending.map( adapter => 
+                <AdapterItem adapter={adapter} restartAdapter={restartAdapter} />
             )}
         </PageFrame>
     )
